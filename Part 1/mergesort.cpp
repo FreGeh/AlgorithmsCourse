6b6db5bf139c2818630976f3a4362e4018b98e3e
FreGeh <fregeh7@gmail.com>
2023-06-27 13:52:07 +0200

    started in CLion

--- Part 1/mergesort.cpp
+++ Part 1/mergesort.cpp
@@ -2,14 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello World";
+    std::cout << "Hello World" << std::endl;
     return 0;
-    int peter = 100;
-    int klara = 5;
-    int tmp;
-    tmp = peter;
-    std::cout << "1: tmp" << tmp;
-    peter = klara;
-    tmp = peter;
-    std::cout << "tmp" << tmp;
 }
